7729f66b5cf8c2eeedfeb586f1ae4ec9808fb01e
Saga Kortesaari <sagak@student.chalmers.se>
2021-07-31 21:52:53 +0200

    deleted unnecessary stuff

--- feelings/src/App.tsx
+++ feelings/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import styled from "styled-components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <p>how are you feeling today?</p>
+            <form>
+                <input
+                    type="text"
+                    placeholder="type here + press enter"
+                    id="inputbox"
+                    name="feeling"
+                />
+            </form>
+        </>
+    );
 }
 
 export default App;
