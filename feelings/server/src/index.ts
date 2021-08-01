c4f24c01ee09293f136d6e0bbe474166da124281
Saga Kortesaari <sagak@student.chalmers.se>
2021-08-01 16:31:42 +0200

    added simple express API + two endpoints for getting/inserting feelings

--- feelings/server/src/index.ts
+++ feelings/server/src/index.ts
@@ -1,21 +1,39 @@
-import { MikroORM } from "mikro-orm";
-//import express from "express";
-import config from "./mikro-orm-config";
+import { MikroORM, RequestContext } from "mikro-orm";
+import express from "express";
+import config from "./mikro-orm.config";
 import { Feelings } from "./entities/Feelings";
 
 async function main() {
-    console.log("hiii :)");
     const orm = await MikroORM.init(config);
 
-    //const app = express();
+    const app = express();
+    app.use(express.json());
 
-    /*
     app.use((req, res, next) => {
-        RequestContext.create(orm.em, next);
-    }); */
+        if (req.headers.authorization == process.env.API_PASS) {
+            RequestContext.create(orm.em, next);
+        } else {
+            res.send("invalid");
+        }
+    });
 
-    const feelings = await orm.em.find(Feelings, {});
-    console.log(feelings);
+    app.post("/feeling", async (req, res) => {
+        orm.em.persistAndFlush(
+            orm.em.create(Feelings, {
+                feeling: req.body.feeling,
+            })
+        );
+
+        res.send("inserted feeling");
+    });
+
+    app.get("/getFeelings", async (_, res) => {
+        res.send(await orm.em.find(Feelings, {}));
+    });
+
+    app.listen(3000, () => {
+        console.log(`Example app listening at http://localhost:3000`);
+    });
 }
 
 main();
